27b62ac68466985f860bfe236bb5dc16c05468bc
Yurii Rohoza <unixexp@gmail.com>
2024-02-22 16:32:39 +0200

    Updated

---
+++ youamp/README.txt
@@ -0,0 +1,3 @@
+Run example
+
+/Users/unixexp/Soft/jdk/jdk-23.jdk/Contents/Home/bin/java -Djava.library.path=/Users/unixexp/Soft/jdk/javafx-sdk-23/lib -jar ./youamp-1.0-SNAPSHOT.jar
--- youamp/src/main/java/com/youamp/YouAMP.java
+++ youamp/src/main/java/com/youamp/YouAMP.java
@@ -1,20 +1,46 @@
 package com.youamp;
 
-import com.youamp.gui.MainWindow;
 import com.youamp.media.youtube.TestYoutubeMediaManager;
 import com.youamp.media.youtube.VideoMeta;
 import com.youamp.media.youtube.YoutubeMediaManager;
 import com.youamp.media.youtube.YtFile;
 
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
 import java.util.Iterator;
 import java.util.Map;
 
-public class YouAMP {
+public class YouAMP extends Application {
 
     public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow();
-        mainWindow.create();
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        VBox vBox = new VBox(new Label("Main"));
+        Scene scene = new Scene(vBox);
+
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        System.out.println(primaryScreenBounds.getWidth());
+        System.out.println(primaryScreenBounds.getHeight());
+
+        stage.setWidth(400);
+        stage.setHeight(200);
+        stage.setScene(scene);
+        stage.show();
+
+        loadMedia();
+    }
+
+    public void loadMedia() {
         final String youtubeLink = "https://www.youtube.com/watch?v=tBN7zJ-QdUQ";
         // YoutubeMediaManager youtubeMediaManager = new YoutubeMediaManager() {
         TestYoutubeMediaManager youtubeMediaManager = new TestYoutubeMediaManager() {
@@ -36,7 +62,5 @@ public class YouAMP {
             }
         };
         youtubeMediaManager.load(youtubeLink);
-
     }
-
 }
